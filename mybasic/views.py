8c767621a3efcdacf7e28ef351f6c2385325f651
ilhamfadillah <ilhamfadillah1995@gmail.com>
2019-07-23 23:11:10 +0700

    create and read in person app

--- mybasic/views.py
+++ mybasic/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
 
 def index(request):
     return HttpResponse("Hello, this is my first app in django")
+
+def create(request):
+    person = Person()
+    person.first_name = "testing"
+    person.last_name = "account"
+    person.save()
+    return HttpResponse("This is create view")
+
+def read(request):
+    persons = Person.objects.all()
+    for person in persons:
+        text = person.first_name +" "+person.last_name
+        return HttpResponse(text)
